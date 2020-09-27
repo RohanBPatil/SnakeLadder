68a038e677dfe43a95729afd706f078ea74dc429
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-27 18:45:27 +0530

    Player 1 rolls dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,7 @@ package SnakeLadderPackage;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		int player1_position = 0;
+		int dice_num = (int)Math.floor(Math.random() * 10) % 6 + 1 ;
+		System.out.println("Number on dice : "+dice_num);
 	}
 }
